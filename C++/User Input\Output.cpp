2446327195305e4397de81bdf69f6d5ea0d5a1d4
Aashin A P <118371201+Aashin-A-P@users.noreply.github.com>
2024-07-27 10:11:05 +0530

    Add files via upload

---
+++ C++/User Input\Output.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "Enter your name:" ;
+    string str;
+    getline(cin,str);
+    cout << "Hello "<< str << " Welcome to the Striver's DSA sheet" <<endl;
+    return 0;
+}
