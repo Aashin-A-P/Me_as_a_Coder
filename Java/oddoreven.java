23427997240babf72d81eee92d54a9e1e1754549
Aashin A P <118371201+Aashin-A-P@users.noreply.github.com>
2024-08-10 10:29:44 +0530

    Add files via upload

--- Java/oddoreven.java
+++ Java/oddoreven.java
@@ -1,16 +1,13 @@
 import java.util.Scanner;
-public class oddoreven{
+public class oddoreven {
     public static void main(String[] args) {
-        int n;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a no:");
-        n = sc.nextInt();
-        if(n%2==0){
-            System.out.println(n+" is an even no");
-        }
-        else{
-            System.out.println(n+" is an odd no");
-        }
+        int no = sc.nextInt();
         sc.close();
-    }
+        if(no%2==0)
+            System.out.println(no+" is an even no");
+        else    
+            System.out.println(no+" is an odd no");
+    }    
 }
