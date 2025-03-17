9203eafa9590de49e8b57fbf9ad556b09b864352
Aashin A P <apaashin@gmail.com>
2025-03-17 22:43:32 +0530

    Add variables.java to Basics package

---
+++ Java/Basics/variables.java
@@ -0,0 +1,11 @@
+package Basics;
+
+public class variables {
+    public static void main(String[] args) {
+        int num = 1000;
+        System.out.println(num);
+        for(int i=0;i<num;i+=10){
+            System.out.println(i);
+        }
+    }
+}
